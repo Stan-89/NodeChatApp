8f64b3e01a990837a8932c1e8d5799f5556a5f2b
Stan-89 <stanislavVitekov@gmail.com>
2018-04-19 12:00:53 -0400

    Basic chatroom

--- public/js/index.js
+++ public/js/index.js
@@ -15,10 +15,31 @@ socket.on('newMessage', function(message) {
 });
 
 
-//Just a test to see how we can use the event acknowledgement from the server
-socket.emit('createMessage', {
-  from: 'Frank',
-  text: 'Hi'
-}, function (data) {
-  console.log('Got it', data);
+//Since we have jquery
+//On submission of the message form
+jQuery('#message-form').on('submit', function(e){
+  //Don't let the page reload
+  e.preventDefault();
+
+  //Emit the message
+  //Recall that socket = io(); and it represents this user's connection to the
+  //server
+  //Also since JS, we have access to jQuery at the same time as socket.js
+  //that is given from server
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function(){
+
+  });
+});
+
+//Now, listening to events from server (newMessage), we print it
+//Recall that we avoid using arrow notation on user side since many have no es6
+socket.on('newMessage', function(message){
+console.log('newMessage', message);
+var li = jQuery('<li></li>');
+//Concatination works even if no es6
+li.text(`${message.from}: ${message.text}`);
+jQuery('#messages').append(li);
 });
