c0389c5cd21381496dff98ae2f96ec06385bcb67
Stan-89 <stanislavVitekov@gmail.com>
2018-04-23 17:59:46 -0400

    Update user classes

--- server/util/users.js
+++ server/util/users.js
@@ -15,6 +15,38 @@ class Users
       return user;
     }
 
+    //Get a user based on an id
+    //Recall that someArray.filter((eachEntry) => someConditionWithEachEntry)
+    //Will return an array of those results
+    getUser(id)
+    {
+      return this.users.filter((user) => user.id === id)[0]; //Taking the first element
+    }
+
+    //Remove a user based on an id
+    removeUser(id)
+    {
+      //First, obtain the said user
+      var user = this.getUser(id);
+
+      //If it did return something (not an empty array)
+      if(user){
+        this.users = this.users.filter((user) => user.id !== id);
+      }
+
+      return user;
+    }
+
+    //Get all of the users in a specific room
+    getUserList(room)
+    {
+      //Get the users who fit the criteria
+      var users = this.users.filter((user) => user.room === room);
+      //And now we're just taking the names for this
+      var namesArray = users.map((user) => user.name);
+      return namesArray;
+    }
+
 }
 
 
--- server/util/users.test.js
+++ server/util/users.test.js
@@ -8,8 +8,32 @@ const {Users} = require('./users.js');
 //Describe it
 describe('Testing Users', () => {
 
+    //Before each for each execution of tests
+    beforeEach(() => {
+      users = new Users();
+      users.users = [
+        {
+          id: '1',
+          name: 'Stannis',
+          room: 'Room 1',
+        },
+        {
+          id: '2',
+          name: 'TheMannis',
+          room: 'Room 1',
+        },
+        {
+          id: '3',
+          name: 'John',
+          room: 'Room 2',
+        },
+    ];
+    });
+
+
+
     //Should add a new user
-    it('Should add a new user', () => {
+  it('Should add a new user', () => {
       var users = new Users();
 
 
@@ -26,4 +50,56 @@ describe('Testing Users', () => {
     expect(users.users).toEqual([user]);
 
   });
+
+  //Removing a user
+  it('should remove a user', () => {
+      var userId = '1';
+      var user = users.removeUser(userId);
+
+      expect(user.id).toBe(userId);
+      expect(users.users.length).toBe(2);
+    });
+
+    //Not removing since no such id
+    it('should not remove user', () => {
+      var userId = '99';
+      var user = users.removeUser(userId);
+
+      expect(typeof user).toBe('undefined');
+      expect(users.users.length).toBe(3);
+    });
+
+
+    //Should return since id is there
+    it('should find user', () => {
+      var userId = '2';
+      var user = users.getUser(userId);
+
+      expect(user.id).toBe(userId);
+    });
+
+    //Not since no ID
+    it('should not find user', () => {
+      var userId = '99';
+      var user = users.getUser(userId);
+
+      expect(typeof user).toBe('undefined');
+    });
+
+    //People from room 1
+    it('should return names for Room 1', () => {
+      var userList = users.getUserList('Room 1');
+
+      expect(userList[0]).toEqual('Stannis');
+      expect(userList[1]).toEqual('TheMannis');
+    });
+
+    it('should return names for Room 2', () => {
+      var userList = users.getUserList('Room 2');
+
+      expect(userList).toEqual(['John']);
+    });
+
+
+
 });
